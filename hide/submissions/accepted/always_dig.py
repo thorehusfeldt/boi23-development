f4003bce226eda7e0610a721cd1c3a054d749936
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-02-19 13:54:56 +0100

    [hide] Add simple submission (should eventually WA)
    always-dig.py stoically walks right and digs in whenever
    it as to. Currently in submissions/accepted to make verifyproblem
    happy; needs to be moved to submissions/wrong_answer eventually.

---
+++ hide/submissions/accepted/always_dig.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+""" Wrong solution that stoically walks right,
+    digging a fresh hole whenever necessary.
+"""
+
+z, m, c, n = map(int, input().split())
+holes = set(map(int, (input() for _ in range(n))))
+
+time = pos = 0
+while pos != z:
+    if time % m != 0 and pos not in holes:
+        time += c
+    pos += 1
+    time += 1
+print(time)
