8e228f2f52b3abc65a4c3b18ec4480db175c8821
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-02-19 19:45:19 +0100

    [hide] Make O(zm) time DP sub temporary AC
    Will be demoted to PAC later, this is just
    here to have something to play with.
    Demotes greedy solution to WA

--- hide/submissions/wrong_answer/always_dig.py
+++ hide/submissions/wrong_answer/always_dig.py
@@ -9,7 +9,7 @@ holes = set(map(int, (input() for _ in range(n))))
 
 time = pos = 0
 while pos != z:
-    if time % m != 0 and pos not in holes:
+    if time > 0 and time % m == 0 and pos not in holes:
         time += c
     pos += 1
     time += 1
