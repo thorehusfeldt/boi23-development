c43a4f3fb11a3d30b8925fa1ace13d5746e32879
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-02-19 22:26:06 +0100

    [hide] test group with larger cases
    Sucessfully distinguishes both Lucas’ O(n^2) and Thore’s
    O(zm) submissions from AC(100)

--- hide/data/generate.sh
+++ hide/data/generate.sh
@@ -4,7 +4,7 @@
 
 . ../../_testdata_tools/gen.sh
 
-use_solution th-zm.py 
+use_solution lucas_impl_seg.cpp
 
 # all the generators are introduced here
 compile gen_random.py
@@ -17,7 +17,7 @@ sample 3
 sample 4
 
 # First test group, and the number of points for it
-group group1 100
+group group1 50
 tc 1
 tc 2
 tc 3
@@ -28,3 +28,13 @@ tc ty-some-holes   gen_random -z   10 -m 2 -c 10 -n 3
 tc sm-some-holes   gen_random -z  100 -m 2 -c 10 -n 33
 tc md-some-holes   gen_random -z 1000 -m 2 -c 10 -n 333
 tc md-early-holes  gen_random -z 1000 -m 2 -c 10 -n 333 --max_a 500
+
+group group2 50
+include_group group1
+tc lg-some-holes   gen_random -z 10000 -m 2 -c 10 -n 3333
+tc hg-some-holes   gen_random -z 100000 -m 2 -c 10 -n 33333
+tc hg-large-m      gen_random -z 100000 -m 20000 -c 10 -n 33333
+tc hg-no-holes     gen_random -z 100000 -m 2 -c 10 -n 0
+tc hg-all-holes    gen_random -z 100000 -m 2 -c 10 -n 99999
+
+
