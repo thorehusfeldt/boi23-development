189502b520270055906d8efa3a59a9db586900ee
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-02-20 21:17:50 +0100

    [hide] Make input validator aware of subtask constraints

--- hide/input_format_validators/validate.py
+++ hide/input_format_validators/validate.py
@@ -3,13 +3,23 @@
 import re
 import sys
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser("Validate input to hide")
+parser.add_argument("--max_z", type=int, default=10**12)
+parser.add_argument("--max_m", type=int, default=10**12)
+parser.add_argument("--max_c", type=int, default=10**6)
+parser.add_argument("--max_n", type=int, default=10**5)
+args = parser.parse_args()
+
+
 line = sys.stdin.readline()
 assert re.match(r"[1-9][0-9]* [1-9][0-9]* (0|[1-9][0-9]*) (0|[1-9][0-9]*)\n", line), line
 z, m, c, n  = map(int, line.split())
-assert 0 < z <= 10**12
-assert 0 < m <= 10**12
-assert 0 <= c <= 10**6
-assert 0 <= n <= 10**5
+assert 0 < z <= args.max_z
+assert 0 < m <= args.max_m
+assert 0 <= c <= args.max_c
+assert 0 <= n <= args.max_n
 
 a = []
 for _ in range(n):
