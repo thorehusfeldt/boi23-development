09fe113afef080da3216908a3bb7443a05ae45f5
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-02-21 10:16:59 +0100

    [manhattan] Add simple instances in subtask 1

--- manhattan/data/generate.sh
+++ manhattan/data/generate.sh
@@ -12,6 +12,15 @@ compile gen_explicit.py
 samplegroup
 # no samples
 
-group group1 100
-tc ukr_task_description_sample_1 gen_explicit 4 5 350 1 2 3 3 2 5
+group group1 6
+tc one-point-1 gen_explicit 10 10 350 5 5
+tc one-point-2 gen_explicit 10 10 350 9 1
+tc one-point-3 gen_explicit 10 10 350 1 8
+tc one-point-4 gen_explicit 10 10 350 0 0
+tc one-point-5 gen_explicit 10 10 350 10 0
+tc one-point-6 gen_explicit 10 10 350 0 10
+tc one-point-7 gen_explicit 10 10 350 10 10
 tc ukr_task_description_sample_2 gen_explicit 5 5 350 1 1
+
+group group2 93
+tc ukr_task_description_sample_1 gen_explicit 4 5 350 1 2 3 3 2 5
