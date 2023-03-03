4626ea24bf7fc212b01d71d8c1b929e4d6006c2f
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-03 17:17:55 +0100

    [security] generate and verify path graphs

--- securitycheck/data/generate.sh
+++ securitycheck/data/generate.sh
@@ -16,24 +16,29 @@ sample 1
 
 # First test group, and the number of points for it
 group group1 25
-limits -n 10
+limits --max_n 10
 tc 1
 for F in ../manual_inputs/*.in; do
 	tc_manual $F
 done
 
-
 group group2 24
-limits -n 10000 --structure "star"
+limits --max_n 10000 --structure "path"
+tc sm-path     gen_star -n 100   --degree 1
+tc md-path     gen_star -n 1000  --degree 1
+tc lg-path-999 gen_star -n 999   --degree 1
+tc lg-path     gen_star -n 10000 --degree 1
+
+group group3 23
+limits --max_n 10000 --structure "starlike"
+include_group group2
 tc sm-star gen_star     -n 100
 tc md-star gen_star     -n 1000
 tc sm-starlike gen_star -n 100   --degree 10
 tc md-starlike gen_star -n 1000  --degree 100
-tc md-line     gen_star -n 1000 --degree 1
+tc lg-starlike gen_star -n 10000 --degree 3
 
-group group3 51
-tc 1
+group group4 28
+include_group group3
 tc lg-random gen_random -n 10000
 tc lg-star gen_star -n 10000
-tc lg-starlike gen_star -n 10000 --degree 3
-tc lg-line     gen_star -n 10000 --degree 1
--- securitycheck/input_format_validators/validate.py
+++ securitycheck/input_format_validators/validate.py
@@ -3,13 +3,19 @@
 import re
 import sys
 
-MAX_N = 10000
+from argparse import ArgumentParser
+
+parser = ArgumentParser("Validate input")
+parser.add_argument("--max_n", type=int, default=1000000)
+parser.add_argument("--structure", choices=["path", "starlike"])
+args = parser.parse_args()
 
 line = sys.stdin.readline()
 assert re.match("[1-9][0-9]*\n", line), line
 n = int(line)
-assert 0 < n <= MAX_N
+assert 0 < n <= args.max_n
 
+V = list(range(1, n + 1))
 edges = set()
 halls = set([1])
 for _ in range(n - 1):
@@ -24,13 +30,18 @@ for _ in range(n - 1):
     halls.add(v)
 
 assert len(edges) == n - 1, f"{n - 1} different corridors expected"
-assert n == 1 or halls == set(range(1, n + 1)), "Unexpected set of hall names"
+assert n == 1 or halls == set(V), "Unexpected set of hall names"
 
-V = list(range(1, n))
 E = {i: [] for i in range(1, n + 1)}
 for u, v in edges:
     E[u].append(v)
     E[v].append(u)
+
+if args.structure == "path":
+    assert sum(1 for v in V if len(E[v]) == 1) == 2, "Expected a path"
+
+if args.structure is not None and args.structure == "starlike":
+    assert sum(1 for v in V if len(E[v]) > 2) <= 1, "Expected a starlike graph"
 
 Q = [1]
 visited = set([1])
@@ -44,6 +55,8 @@ while Q:
             R.append(v)
     Q = R
 assert len(visited) == n, "Expected a connected graph"
+
+
 
 assert sys.stdin.readline() == "", "Extra input"
 sys.exit(42)
