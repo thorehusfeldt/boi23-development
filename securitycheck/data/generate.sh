fb7572e942e1a1b0d51d5b2c4f07617cea0f53bb
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-03 10:37:40 +0100

    [security] Generate line graphs

--- securitycheck/data/generate.sh
+++ securitycheck/data/generate.sh
@@ -21,16 +21,19 @@ tc 1
 tc ty-random gen_random -n 10
 tc ty-star gen_star -n 10
 tc ty-starlike gen_star -n 10 --degree 3
+tc ty-line gen_star -n 10 --degree 1
 
 group group2 24
 limits -n 10000 --structure "star"
-tc sm-star gen_star   -n 100
-tc md-star gen_star   -n 1000
-tc sm-starlike gen_star   -n 100   --degree 10
-tc md-starlike gen_star   -n 1000  --degree 100
+tc sm-star gen_star     -n 100
+tc md-star gen_star     -n 1000
+tc sm-starlike gen_star -n 100   --degree 10
+tc md-starlike gen_star -n 1000  --degree 100
+tc md-line     gen_star -n 1000 --degree 1
 
 group group3 51
 tc 1
 tc lg-random gen_random -n 10000
 tc lg-star gen_star -n 10000
 tc lg-starlike gen_star -n 10000 --degree 3
+tc lg-line     gen_star -n 10000 --degree 1
