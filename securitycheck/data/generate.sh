c7f4fa2605bdc294cc72de27c22e9ec073fe7d76
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-05 11:37:11 +0100

    [security] use n=100000 in test groups

--- securitycheck/data/generate.sh
+++ securitycheck/data/generate.sh
@@ -4,7 +4,7 @@
 
 . ../../_testdata_tools/gen.sh
 
-use_solution dacin21-full-two_dfs.py
+use_solution th.py
 
 # all the generators are introduced here
 compile gen_random.py
@@ -24,39 +24,46 @@ for F in ../manual_inputs/*.in; do
 done
 
 group group2 19
-limits --max_n 10000 --structure "path"
+limits --max_n 100000 --structure "path"
 tc sm-path     gen_star -n 100   --degree 1
 tc md-path     gen_star -n 1000  --degree 1
 tc lg-path-999 gen_star -n 999   --degree 1
-tc lg-path     gen_star -n 10000 --degree 1
+tc lg-path     gen_star -n 10000  --degree 1
+tc hg-path     gen_star -n 100000 --degree 1
+tc hg-path-1   gen_star -n 99999  --degree 1
 
 group group3 20
-limits --max_n 10000 --structure "starlike"
+limits --max_n 100000 --structure "starlike"
 include_group group2
 tc sm-star gen_star     -n 100
 tc md-star gen_star     -n 1000
 tc sm-starlike gen_star -n 100   --degree 10
 tc md-starlike gen_star -n 1000  --degree 100
-tc lg-starlike gen_star -n 10000 --degree 3
+tc lg-starlike      gen_star -n  10000 --degree 3
+tc hg-starlike-3    gen_star -n 100000 --degree 3
+tc hg-starlike-30   gen_star -n 100000 --degree 30
+tc hg-starlike-300  gen_star -n 100000 --degree 300
+tc hg-starlike-3000 gen_star -n 100000 --degree 3000
 
 group group4 21
-limits --max_n 10000
-include_group group3
-tc lg-random     gen_random -n 10000
-tc lg-star       gen_star -n 10000
-tc lg-2-regular  gen_regular   -n 10000 -d 2
-tc lg-3-regular-1  gen_regular -n 10000 -d 3
-tc lg-3-regular-2  gen_regular -n  9999 -d 3
-tc lg-3-regular-3  gen_regular -n  9998 -d 3
-tc lg-10-regular gen_regular   -n 10000 -d 10
-tc lg-100-regular gen_regular  -n 10000 -d 100
+limits --max_n 1000
+include_group group1
+tc md-random     gen_random -n 1000
+tc md-2-regular  gen_regular   -n 1000 -d 2
+tc md-3-regular-1  gen_regular -n 1000 -d 3
+tc md-3-regular-2  gen_regular -n  999 -d 3
+tc md-3-regular-3  gen_regular -n  998 -d 3
+tc md-10-regular gen_regular   -n 1000 -d 10
+tc md-100-regular gen_regular  -n 1000 -d 100
 
 group group5 22
-limits --max_n 200000
+limits --max_n 100000
+include_group group2
+include_group group3
 include_group group4
-tc hg-random     gen_random     -n 200000
-tc hg-star       gen_star       -n 200000
-tc hg-2-regular  gen_regular    -n 200000 -d 2
-tc hg-10-regular gen_regular    -n 200000 -d 10
-tc hg-100-regular gen_regular   -n 200000 -d 100
-tc hg-1000-regular gen_regular  -n 200000 -d 1000
+tc hg-random     gen_random     -n 100000
+tc hg-star       gen_star       -n 100000
+tc hg-2-regular  gen_regular    -n 100000 -d 2
+tc hg-10-regular gen_regular    -n 100000 -d 10
+tc hg-100-regular gen_regular   -n 100000 -d 100
+tc hg-1000-regular gen_regular  -n 100000 -d 1000
