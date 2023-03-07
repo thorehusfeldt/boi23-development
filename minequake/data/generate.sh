6374542196bd12a445891e2f5cb11f8dc8a35808
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-07 12:22:39 +0100

    [minequake] Generate more large instances

--- minequake/data/generate.sh
+++ minequake/data/generate.sh
@@ -22,6 +22,7 @@ group group1 18
 limits --max_n 100000 --structure "path"
 tc 1
 tc 3
+tc edge        gen_star -n 2     --degree 1
 tc sm-path     gen_star -n 100   --degree 1
 tc md-path     gen_star -n 1000  --degree 1
 tc lg-path-999 gen_star -n 999   --degree 1
@@ -33,15 +34,17 @@ group group2 19
 limits --max_n 100000 --structure "starlike"
 include_group group1
 tc 2
-tc sm-star gen_star     -n 100
-tc md-star gen_star     -n 1000
-tc sm-starlike gen_star -n 100   --degree 10
-tc md-starlike gen_star -n 1000  --degree 100
+tc sm-star gen_star          -n 100
+tc md-star gen_star          -n 1000
+tc hg-star gen_star          -n 100000
+tc sm-starlike gen_star      -n 100   --degree 10
+tc md-starlike gen_star      -n 1000  --degree 100
 tc lg-starlike      gen_star -n  10000 --degree 3
 tc hg-starlike-3    gen_star -n 100000 --degree 3
 tc hg-starlike-30   gen_star -n 100000 --degree 30
 tc hg-starlike-300  gen_star -n 100000 --degree 300
 tc hg-starlike-3000 gen_star -n 100000 --degree 3000
+tc hg-starlike-30K  gen_star -n 100000 --degree 30000
 
 group group3 20
 limits --max_n 10
@@ -60,8 +63,11 @@ done
 group group4 21
 limits --max_n 1000
 include_group group3
-tc md-random     gen_random -n 1000
-tc md-2-regular  gen_regular   -n 1000 -d 2
+tc md-random     gen_random    -n 1000
+tc md-random-1     gen_random  -n 1000
+tc md-random-2    gen_random   -n 1000
+tc md-2-regular-1  gen_regular -n 1000 -d 2
+tc md-2-regular-2  gen_regular -n  999 -d 2
 tc md-3-regular-1  gen_regular -n 1000 -d 3
 tc md-3-regular-2  gen_regular -n  999 -d 3
 tc md-3-regular-3  gen_regular -n  998 -d 3
@@ -73,8 +79,9 @@ limits --max_n 100000
 include_group group1
 include_group group2
 include_group group4
-tc hg-random     gen_random     -n 100000
-tc hg-star       gen_star       -n 100000
+tc hg-random-1   gen_random     -n 100000
+tc hg-random-2   gen_random     -n 100000
+tc hg-random-3   gen_random     -n 100000
 tc hg-2-regular  gen_regular    -n 100000 -d 2
 tc hg-10-regular gen_regular    -n 100000 -d 10
 tc hg-100-regular gen_regular   -n 100000 -d 100
