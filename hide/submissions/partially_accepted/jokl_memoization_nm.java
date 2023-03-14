e28a5c6912032fc11592194d0a5bb4125dec9023
Jonas Klausen <k.jonas@hotmail.com>
2023-03-14 14:36:42 +0100

    [hide] Add memoization to brute-force solution

---
+++ hide/submissions/partially_accepted/jokl_memoization_nm.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class jokl_memoization_nm{
+	static int[] caves;
+	static int z, m, c, n;
+
+	// Let memo[i][k] be the minimum cost of reaching z
+	// starting in caves[i] at a time t = k mod m
+	static int[][] memo;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		z = sc.nextInt();
+		m = sc.nextInt();
+		c = sc.nextInt();
+		n = sc.nextInt() + 1;
+		
+		memo = new int[n][m];
+		caves = new int[n];
+		// Implicitly let pos. 0 be a cave
+		for(int i = 1; i<n; i++) {
+			caves[i] = sc.nextInt();
+		}
+		
+		System.out.println(explore(0, 0));
+	}
+	
+	// Return damage suffered when traveling from caves[cave]
+	// either taking off at time `time` (go-solution)
+	// or waiting for next pulse (wait-solution)
+	static int explore(int time, int cave) {
+		if(memo[cave][time % m] != 0)
+			return memo[cave][time % m];
+
+		if(cave == (n-1)) {
+			int go = damage(time, z-caves[cave]);
+			int pulseTime = m * (time/m) + m;
+			int wait = damage(pulseTime, z-caves[cave]) + (pulseTime-time);
+			int sol = Math.min(go, wait);
+			memo[cave][time % m] = sol;
+			return sol;
+		}
+		int dist = caves[cave+1] - caves[cave];
+		int go = damage(time, dist) + explore(time+dist, cave+1);
+		int pulseTime = m * (time/m) + m;
+		int wait = damage(pulseTime, dist) + (pulseTime-time) + explore(pulseTime+dist, cave+1);
+		int sol = Math.min(go, wait);
+		memo[cave][time % m] = sol;
+		return sol;
+	}
+	
+	// Damage received when traveling `dist` distance starting at time `time`,
+	// assuming that final location is a cave
+	static int damage(int time, int dist) {
+		int pulses = (time+dist-1)/m - time/m;
+		return c*pulses + dist;
+	}
+}
