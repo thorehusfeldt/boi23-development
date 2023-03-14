0ec2ea5192a5de2a348893165d3d2cb9f4c6b0af
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-14 21:04:25 +0100

    [hide] tweaks to problem statement, WA sub

--- hide/submissions/wrong_answer/always_dig.py
+++ hide/submissions/wrong_answer/always_dig.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python3
 
-""" Wrong solution that stoically drives home
+""" Wrong solution that stoically drives home,
     taking whatever damage the pulsar throws at you.
 """
 
 z, m, c, n = map(int, input().split())
-holes = set(map(int, (input() for _ in range(n))))
+shelters = set(map(int, (input() for _ in range(n))))
 
 damage = pos = time = 0
 while pos != z:
-    if time > 0 and time % m == 0 and pos not in holes:
+    if time > 0 and time % m == 0 and pos not in shelters:
         damage += c
     time += 1
     pos += 1
