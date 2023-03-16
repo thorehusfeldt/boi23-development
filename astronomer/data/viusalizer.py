08a866dd473ea0e82b9afe8f955bae4f4f95e636
Elias Lolck <elias@lolck.com>
2023-03-16 18:18:43 +0100

    Astronomer: Added a visualizing tool for cost at position

---
+++ astronomer/data/viusalizer.py
@@ -0,0 +1,63 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D  
+# Axes3D import has side effects, it enables using projection='3d' in add_subplot
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import random
+
+k, n, s, t = map(int, input().split())
+
+inf = 10**20
+
+mnx = inf
+mxx = -inf
+mny = inf
+mxy = -inf
+ans = inf
+
+points = set()
+for i in range(n):
+    x, y = map(int, input().split())
+    points.add((x,y))
+    mnx = min(mnx, x)
+    mxx = max(mxx, x)
+    mny = min(mny, y)
+    mxy = max(mxy, y)
+
+
+def fun(_x, _y):
+    global ans
+    ret = []
+    for x, y in zip(_x, _y):
+        cost = 0
+        cost += (x*x + y*y)**0.5*s
+        dist = []
+        for x1, y1 in points:
+            dist.append(((x-x1)**2 + (y-y1)**2)**0.5)
+        dist = sorted(dist)
+        ans = min(ans, cost + dist[k-1]*t)
+        ret.append((cost + dist[k-1]*t))
+    return ret
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.axis([2*mnx,2*mxx,2*mny,2*mxy])
+x = np.arange(2*mnx, 2*mxx, (mxx-mnx)/300)
+y = np.arange(2*mny, 2*mxy, (mxy-mny)/300)
+X, Y = np.meshgrid(x, y)
+zs = np.array(fun(np.ravel(X), np.ravel(Y)))
+Z = zs.reshape(X.shape)
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.inferno, linewidth=0, antialiased=False)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+ax.invert_zaxis()
+
+print(ans)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+
