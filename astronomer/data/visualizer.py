7b75086130ddedaf26f55ed2ad0a4e0dcc14f857
Slemmie <thorvejeneriksen@gmail.com>
2023-03-18 16:27:10 +0100

    add peak counting to visualizer

--- astronomer/data/visualizer.py
+++ astronomer/data/visualizer.py
@@ -25,19 +25,34 @@ for i in range(n):
     mny = min(mny, y)
     mxy = max(mxy, y)
 
+count_peaks = 0
 
 def fun(_x, _y):
     global ans
+    global count_peaks
     ret = []
     for x, y in zip(_x, _y):
-        cost = 0
-        cost += (x*x + y*y)**0.5*s
-        dist = []
-        for x1, y1 in points:
-            dist.append(((x-x1)**2 + (y-y1)**2)**0.5)
-        dist = sorted(dist)
-        ans = min(ans, cost + dist[k-1]*t)
-        ret.append((cost + dist[k-1]*t))
+        def get_cost(X, Y):
+            global ans
+            cost = 0
+            cost += (X*X + Y*Y)**0.5*s
+            dist = []
+            for x1, y1 in points:
+                dist.append(((X-x1)**2 + (Y-y1)**2)**0.5)
+            dist = sorted(dist)
+            ans = min(ans, cost + dist[k-1]*t)
+            return cost + dist[k-1]*t
+        cost_here = get_cost(x,y)
+        ret.append(cost_here)
+        def check_peak():
+            is_peak = True
+            dx = [ .5, -.5, 0., 0. ]
+            dy = [ 0., 0., .5, -.5 ]
+            for i in range(4):
+                is_peak &= cost_here <= get_cost(x+dx[i], y+dy[i])
+            global count_peaks
+            count_peaks += is_peak
+        check_peak() # comment out for significant speed up
     return ret
 
 fig = plt.figure()
@@ -58,6 +73,7 @@ ax.set_zlabel('Z Label')
 ax.invert_zaxis()
 
 print(ans)
+print("#peaks:", count_peaks)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
