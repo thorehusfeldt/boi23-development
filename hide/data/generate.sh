5c82469d908b2038c3a18d656d3311ab5ab464a7
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-03-30 16:15:48 +0200

    [hide] Merge groups 1 and 2. (No submissions distinguish them)

--- hide/data/generate.sh
+++ hide/data/generate.sh
@@ -22,25 +22,25 @@ sample 3
 sample 4
 sample 5
 
-group group1 5
-# Loop over binary strings of length 5
-for i in {0,1}{0,1}{0,1}{0,1}{0,1}; do
-    tc small-$i GenSmallComplete $i
-done
+group group1 10
+limits --max_n 10 --max_z 1000 --max_m 100
 
-
-group group2 5
-limits --max_n 10 --max_z 100 --max_m 100
 tc 1
 tc 2
 tc 3
 tc 4
 tc 5
+#
+# Loop over binary strings of length 5
+for i in {0,1}{0,1}{0,1}{0,1}{0,1}; do
+    tc small-$i GenSmallComplete $i
+done
+
 tc sm-two-holes gen_random -z  10 -m  2 -c 10 -n 2
 tc lg-two-holes gen_random -z 100 -m 21 -c 10 -n 2
 tc lg-few-holes gen_random -z 100 -m 21 -c 10 -n 9
 
-group group3 10
+group group2 10
 limits --max_z 100 --max_m 100
 # include_group group1       group1 doesn't respect the small limit on z!
 tc ty-no-holes     gen_random -z   10 -m 2 -c 10 -n 0
@@ -51,7 +51,7 @@ tc ty-packed-early gen_random --strategy regular  -z 100 --min_a  1 --max_a 50 -
 tc ty-packed-late  gen_random --strategy regular  -z 100 --min_a 50 --max_a 99 -n 50 -m 2 -c 10
 tc ty-alternating  gen_random --strategy regular  -z 100 --min_a  1 --max_a 99 -n 50 -m 2 -c 10
 
-group group4 15
+group group3 15
 limits --max_n 1000 --max_m 1000000
 tc md-some-holes   gen_random -z 1000 -m 2 -c 10 -n 333
 tc md-early-holes  gen_random -z 1000 -m 2 -c 10 -n 333 --max_a 500
@@ -60,11 +60,12 @@ tc rd-10k-lg-md    gen_random -z 10000 -m 5112 -c 10 -n 333
 tc rd-10k-lg-1k    gen_random -z 10000 -m 5112 -c 10 -n 1000
 tc sm-n-lg-m       gen_random -z 1000000 -m 100000 -c 10 -n 1000 # need more of these
 tc sm-n-hg-m       gen_random -z 10000000 -m 1000000 -c 10 -n 1000 
+tc sm-crit-smallc  gen_critc  -z 10000 -m 42 -c0 7   -n 100
 tc crit-smallc     gen_critc  -z 10000 -m 42 -c0 7   -n 1000
 tc crit-medc       gen_critc  -z 10000 -m 42 -c0 24  -n 1000
 tc crit-bigc       gen_critc  -z 10000 -m 100 -c0 43 -n 1000
 
-group group5 20
+group group4 20
 limits --max_m 100
 tc small-m-md-early-holes  gen_random  -z 1000000 -m 100 -c 10 -n 100000 --max_a 200000
 tc small-m-rd              gen_random  -z 1000000 -m 100 -c 10 -n 100000
@@ -72,13 +73,12 @@ tc small-m-crit-smallc     gen_critc   -z 1000000 -m 100 -c0 7   -n 1000
 tc small-m-crit-medc       gen_critc   -z 1000000 -m 100 -c0 24  -n 1000
 tc small-m-crit-bigc       gen_critc   -z 1000000 -m 100 -c0 43 -n 1000
 
-group group6 35
+group group5 35
 limits --max_m 1000000
 include_group group1
 include_group group2
 include_group group3
 include_group group4
-include_group group5
 tc hg-midm-1       gen_random -z 10000000 -m 65536 -c 20 -n 100000
 tc hg-midm-2       gen_random -z 100000000 -m 11231 -c 10 -n 99998
 tc hg-midm-3       gen_random -z 10000000000 -m 1000000 -c 42 -n 98976
@@ -89,8 +89,8 @@ tc hg-midm-crit-smallc     gen_critc   -z 1000000000 -m 1000000 -c0 7   -n 10000
 tc hg-midm-crit-medc       gen_critc   -z 1000000000 -m 1000000 -c0 24  -n 100000
 tc hg-midm-crit-bigc       gen_critc   -z 1000000000 -m 1000000 -c0 52  -n 100000
 
-group group7 10
-include_group group6
+group group6 10
+include_group group5
 tc hg-bigm-1       gen_random -z 100000000000 -m 3000000000 -c 20 -n 100000
 tc hg-bigm-2       gen_random -z 1000000000 -m 12345678 -c 10 -n 99998
 tc hg-bigm-3       gen_random -z 10000000000 -m 1001234145 -c 42 -n 98976
