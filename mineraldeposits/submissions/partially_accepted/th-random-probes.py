f4d309293de861a2b4c00ffa5df20d3d480b1d14
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-04-05 09:09:40 +0200

    clean up th-random-probes PAC submission

--- mineraldeposits/submissions/partially_accepted/th-random-probes.py
+++ mineraldeposits/submissions/partially_accepted/th-random-probes.py
@@ -4,24 +4,34 @@ import random
 
 b, k, _ = map(int, input().split())
 
-distances_sw = list(map(int, input(f"? -{b} -{b}\n").split()))
-distances_se = list(map(int, input(f"? {b} -{b}\n").split()))
-candidates = set()
-for d_se in distances_se:
-    for d_sw in distances_sw:
-        y = (d_sw + d_se - 4 * b) // 2
-        x = y + 2 * b - d_se
-        if -b <= x <= b and -b <= y <= b:
-            candidates.add((x,y))
+
+def query(px, py):
+    return map(int, input(f"? {px} {py}\n").split())
+
+
+def distance(p, q):
+    return abs(p[0] - q[0]) + abs(p[1] - q[1])
+
+def candidate(sw, se):
+    y = (sw + se - 4 * b) // 2
+    x = y + 2 * b - se
+    return (x, y) if -b <= x <= b and -b <= y <= b else None
+
+distances_sw = list(query(-b, -b))
+distances_se = list(query(b, -b))
+candidates = set(
+        candidate(sw, se) for sw in distances_sw for se in distances_se
+)
+candidates.discard(None)
 
 candidates = list(candidates)
 deposits = []
 while len(candidates) > 0:
     r = random.choice(candidates)
-    distances = set(map(int, input(f"? {r[0]} {r[1]}\n").split()))
+    distances = set(query(*r))
     if 0 in distances:
         deposits.append(r)
         candidates.remove(r)
-    candidates = [p for p in candidates if abs(p[0] - r[0]) + abs(p[1] - r[1]) in distances]
+    candidates = [p for p in candidates if distance(p, r) in distances]
 
 print("!", " ".join(f"{d[0]} {d[1]}" for d in deposits), flush=True)
