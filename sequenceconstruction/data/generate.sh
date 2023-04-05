29567a29eca833e06418b883c9a5186d92e778a2
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-04-05 17:52:51 +0200

    [sequence…] Test group for uniform weights, n <= 300

--- sequenceconstruction/data/generate.sh
+++ sequenceconstruction/data/generate.sh
@@ -4,7 +4,7 @@
 
 . ../../_tools/gen.sh
 
-use_solution dacin21-memo_vector.cpp
+use_solution dacin21-memo_vector_hash.cpp
 
 # all the generators are introduced here
 compile gen_explicit.py
@@ -26,31 +26,49 @@ tc ty-fibo-back gen_explicit  11 8 5 3 2
 tc ty-random    gen_random    -n 10
 tc ty-random-10 gen_random    -n 10 --max_w 10
 tc ty-random-5  gen_random    -n 10 --max_w 5
-tc ty-random-2  gen_random    -n 10 --max_w 1
+tc ty-ones-2    gen_random    -n 10 --max_w 1
 
-group group2 20
+group group2 10
 limits --max_w 1 --max_n 300
 tc ty-one-one   gen_explicit  1
 tc ty-five-ones  gen_explicit  1 1 1 1 1
 tc sm-all-ones  gen_random -n 300 --max_w 1
+tc ty-all-ones-2  gen_random -n 11 --max_w 1
+tc sm-all-ones-1  gen_random -n 299 --max_w 1
 
-group group3 9
+group group3 10
+limits --same_w --max_n 300
+include_group group2
+tc sm-all-twos  gen_random -n 300 --min_w 2 --max_w 2
+tc sm-all-3s  gen_random   -n 300 --min_w 3 --max_w 3
+tc sm-all-10s  gen_random   -n 300 --min_w 10 --max_w 10
+tc sm-all-100s  gen_random   -n 300 --min_w 100 --max_w 100
+tc sm-all-1_000_000s  gen_random   -n 300 --min_w 1_000_000 --max_w 1_000_000
+tc sm-all-999_999s  gen_random   -n 300 --min_w 999_999 --max_w 999_999
+
+group group4 9
 limits --max_w 1 --max_n 1400
 include_group group2
 tc md-all-ones gen_random -n  301 --max_w 1
 tc lg-all-ones-1 gen_random -n 1399 --max_w 1
 tc lg-all-ones-2 gen_random -n 1400 --max_w 1
 
-group group4 45
+group group5 45
 limits --max_n 10000
 include_group group1
-include_group group3
-tc lg-random gen_random -n 10000
-
-group group5 15
 include_group group4
-tc lg-random-1 gen_random -n 30000
-tc lg-random-2 gen_random -n 30000
-tc lg-random-3 gen_random -n 30000
-tc lg-random-ones gen_random -n 30000 --max_w 1
-tc lg-random-sm gen_random -n 30000 --max_w 10
+tc lg-random-1 gen_random -n 5000 
+tc lg-random-2 gen_random -n 5000
+tc lg-random-3 gen_random -n 5000
+tc lg-random-sm gen_random -n 5000 --min_w 100 --max_w 105
+tc lg-random-ty gen_random -n 5000 --max_w 10
+tc lg-ones      gen_random -n 5000 --max_w 1
+tc lg-random-md gen_random -n 5000 --min_w 1000 --max_w 2000
+
+group group6 15
+include_group group5
+tc hg-random-1  gen_random -n 30000
+tc hg-random-2  gen_random -n 30000
+tc hg-random-3  gen_random -n 30000
+tc hg-ones      gen_random -n 30000 --max_w 1
+tc hg-random-sm gen_random -n 30000 --max_w 10
--- sequenceconstruction/input_format_validators/validate.py
+++ sequenceconstruction/input_format_validators/validate.py
@@ -7,6 +7,8 @@ from argparse import ArgumentParser
 parser = ArgumentParser("Validate input to astronomer")
 parser.add_argument("--max_n", type=int, default=30000)
 parser.add_argument("--max_w", type=int, default=10**6)
+parser.add_argument("--min_w", type=int, default=1)
+parser.add_argument("--same_w", action="store_true")
 args = parser.parse_args()
 
 line = sys.stdin.readline()
@@ -14,12 +16,17 @@ assert re.match(r"[1-9][0-9]*\n", line)
 n = int(line)
 assert n <= args.max_n, (n, args.max_n)
 assert 1 <= n <= 30_000 # constraint:n
+weights = []
 for _ in range(n):
     line = sys.stdin.readline()
     assert re.match(r"[1-9][0-9]*\n", line)
     wi = int(line)
-    assert wi <= args.max_w
+    assert args.min_w <= wi <= args.max_w
     assert 1 <= wi <= 10**6 # constraint:wi
+    weights.append(wi)
+
+if args.same_w:
+    assert len(set(weights)) == 1 # constraint:uniformweights
 
 assert sys.stdin.readline() == "", "Extra input"
 
