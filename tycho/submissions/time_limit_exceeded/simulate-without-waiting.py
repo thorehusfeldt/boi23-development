1eb5c816c9c78da1dba1ec4b9d82669e5a449336
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-04-16 17:14:30 +0200

    `tycho` Move some submissions around

--- tycho/submissions/time_limit_exceeded/simulate-without-waiting.py
+++ tycho/submissions/time_limit_exceeded/simulate-without-waiting.py
@@ -6,13 +6,13 @@
     taking whatever damage the pulsar throws at you.
 """
 
-z, m, c, n = map(int, input().split())
+b, p, d, n = map(int, input().split())
 shelters = set(map(int, (input() for _ in range(n))))
 
 damage = pos = time = 0
-while pos != z:
-    if time > 0 and time % m == 0 and pos not in shelters:
-        damage += c
+while pos != b:
+    if time > 0 and time % p == 0 and pos not in shelters:
+        damage += d
     time += 1
     pos += 1
     damage += 1
